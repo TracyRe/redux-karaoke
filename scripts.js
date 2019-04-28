d2f104db068f4a2c9a122fbe3b6744aa6243a711
Tracy Reith <treith@treith.com>
2019-04-28 12:53:38 -0700

    Set up project, add all code for demo redux project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,212 @@
+
+// LYRIC INFO
+const songList = {
+1: "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', '),
+2: "Twenty-five years and my life is still, Trying to get up that great big hill of hope, For a destination, I realized quickly when I knew I should, That the world was made up of this brotherhood of man, For whatever that means, And so I cry sometimes when I'm lying in bed, Just to get it all out what's in my head, And I, I am feeling a little peculiar, And so I wake in the morning and I step outside, And I take a deep breath and I get real high, and I Scream from the top of my lungs, What's going on?, And I say hey yeah yeah hey yeah yeah, I said hey what's going on?, And I say hey yeah yeah hey yeah yeah,I said hey what's going on?".split(', '),
+3: "I can't seem to face up to the facts, I'm tense and nervous and I can't relax, I can't sleep 'cause my bed's on fire, Don't touch me I'm a real live wire, Psycho Killer Qu'est-ce que c'est, Fa-fa-fa-fa-fa-fa-fa-fa-fa-far better, Run run run run run run run away oh oh, Psycho Killer Qu'est-ce que c'est, Fa-fa-fa-fa-fa-fa-fa-fa-fa-far better, Run run run run run run run away oh oh oh, Yeah yeah yeah yeah!, You start a conversation you can't even finish it, You're talking a lot, but you're not saying anything, When I have nothing to say, my lips are sealed, Say something once, why say it again?, Psycho Killer Qu'est-ce que c'est, Fa-fa-fa-fa-fa-fa-fa-fa-fa-far better, Run run run run run run run away oh oh, Psycho Killer Qu'est-ce que c'est, Fa-fa-fa-fa-fa-fa-fa-fa-fa-far better, Run run run run run run run away oh oh oh, Yeah yeah yeah yeah!".split(', '),
+
+};
+
+// INITIAL REDUX STATE
+const initialState = {
+  currentSongId: null,
+  songsById: {
+    1: {
+      title: "Bye Bye Bye",
+      artist: "N' Sync",
+      songId: 1,
+      songArray: songList[1],
+      arrayPosition: 0,
+    },
+    2: {
+      title: "What's Goin' On",
+      artist: "Four Non-Blondes",
+      songId: 2,
+      songArray: songList[2],
+      arrayPosition: 1,
+    },
+    3: {
+      title: "Psycho Killer",
+      artist: "Talking Heads",
+      songId: 3,
+      songArray: songList[3],
+      arrayPosition: 2,
+    },
+  }
+}
+console.log(initialState);
+
+// REDUCER
+const lyricChangeReducer = (state = initialState.songsById, action) => {
+  let newArrayPosition;
+  let newSongsByIdEntry;
+  let newSongsByIdStateSlice;
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      newArrayPosition = state[action.currentSongId].arrayPosition + 1;
+      newSongsByIdEntry = Object.assign({}, state[action.currentSongId], {
+        arrayPosition: newArrayPosition
+      })
+      newSongsByIdStateSlice = Object.assign({}, state, {
+        [action.currentSongId]: newSongsByIdEntry
+      });
+      return newSongsByIdStateSlice;
+    case 'RESTART_SONG':
+      newSongsByIdEntry = Object.assign({}, state[action.currentSongId], {
+        arrayPosition: 0
+      })
+      newSongsByIdStateSlice = Object.assign({}, state,{
+        [action.currentSongId]: newSongsByIdEntry
+      });
+      return newSongsByIdStateSlice;
+    default:
+      return state;
+  }
+}
+
+const songChangeReducer = (state = initialState.currentSongId, action) => {
+  switch (action.type) {
+    case 'CHANGE_SONG':
+      return  action.newSelectedSongId;
+    default:
+      return state;
+  }
+}
+
+
+const rootReducer = this.Redux.combineReducers({
+  currentSongId: songChangeReducer,
+  songsById: lyricChangeReducer
+});
+
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(rootReducer);
+
+
+// JEST TESTS & SETUP
+const { expect } = window;
+
+expect(lyricChangeReducer(initialState.songsById, { type: null})).toEqual(initialState.songsById);
+
+// expect(lyricChangeReducer(initialState.songsById, { type: 'NEXT_LYRIC', currentSongId: 2 })).toEqual({
+//   1: {
+//     title: "Bye Bye Bye",
+//     artist: "N'Sync",
+//     songId: 1,
+//     songArray: songList[1],
+//     arrayPosition: 0,
+//   },
+//   2: {
+//     title: "What's Goin' On",
+//     artist: "Four Non-Blondes",
+//     songId: 2,
+//     songArray: songList[2],
+//     arrayPosition: 1,
+//   },
+  // 3: {
+  //   title: "Psycho Killer",
+  //   artist: "Talking Heads",
+  //   songId: 3,
+  //   songArray: songList[3],
+  //   arrayPosition: 2,
+  // }
+// });
+
+// expect(lyricChangeReducer(initialState.songsById, { type: 'RESTART_SONG', currentSongId: 1  })).toEqual({
+//   1: {
+//     title: "Bye Bye Bye",
+//     artist: "N'Sync",
+//     songId: 1,
+//     songArray: songList[1],
+//     arrayPosition: 0,
+//   },
+//   2: {
+//     title: "What's Goin' On",
+//     artist: "Four Non-Blondes",
+//     songId: 2,
+//     songArray: songList[2],
+//     arrayPosition: 1,
+//   },
+  // 3: {
+  //   title: "Psycho Killer",
+  //   artist: "Talking Heads",
+  //   songId: 3,
+  //   songArray: songList[3],
+  //   arrayPosition: 2,
+  // }
+// });
+
+expect(songChangeReducer(initialState.currentSongId, { type: 'CHANGE_SONG', newSelectedSongId: 1 })).toEqual(1);
+
+expect(rootReducer(initialState, { type: null })).toEqual(initialState);
+
+
+
+//RENDER STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+
+  if (store.getState().currentSongId) {
+    const currentLine = document.createTextNode(store.getState().songsById[store.getState().currentSongId].songArray[store.getState().songsById[store.getState().currentSongId].arrayPosition]);
+      document.getElementById('lyrics').appendChild(currentLine);
+    } else {
+      const selectSongMessage = document.createTextNode("Select a song from the menu");
+      document.getElementById('lyrics').appendChild(selectSongMessage);
+    }
+  }
+
+const renderSongs = () => {
+  const songsById = store.getState().songsById;
+  for (const songKey in songsById) {
+    const song = songsById[songKey];
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const em = document.createElement('em');
+    const songTitle = document.createTextNode(song.title);
+    const songArtist = document.createTextNode(' by ' + song.title);
+    em.appendChild(songTitle);
+    h3.appendChild(em);
+    h3.appendChild(songArtist);
+    h3.addEventListener('click', function() {
+      selectSong(song.songId);
+    });
+    li.appendChild(h3);
+    document.getElementById('songs').appendChild(li);
+  }
+}
+
+
+// CLICK LISTENER
 const userClick = () => {
-  console.log('click');
+  if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].songArray.length - 1) {
+    store.dispatch({ type: 'RESTART_SONG', currentSongId: store.getState().currentSongId });
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC', currentSongId: store.getState().currentSongId });
+  }
+}
+
+const selectSong = (newSongId) => {
+  let action;
+  if (store.getState().currentSongId) {
+    action = {
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId
+    }
+  } action = {
+    type: 'CHANGE_SONG',
+    newSelectedSongId: newSongId
+  }
+  store.dispatch(action);
+}
+
+// SUBSCRIBE TO REDUX store
+store.subscribe(renderLyrics);
+
+window.onload = function() {
+  renderSongs();
+  renderLyrics();
 }
